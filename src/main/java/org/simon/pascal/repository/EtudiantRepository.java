127b5fa11b82ec40571fbc044184535a114d9140
simonpascal <simon.pascal.ngos@accenture.com>
2018-11-20 16:44:28 +0400

    add other endpoint

--- src/main/java/org/simon/pascal/repository/EtudiantRepository.java
+++ src/main/java/org/simon/pascal/repository/EtudiantRepository.java
@@ -5,11 +5,18 @@ package org.simon.pascal.repository;
 
 import org.simon.pascal.entities.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author simon.pascal.ngos
  *
  */
+@RestResource(path="etudiants",exported=true)
 public interface EtudiantRepository extends JpaRepository<Etudiant, Long>{
-
+	
+	@Query("SELECT MAX(etudiant.matricule) FROM Etudiant etudiant ")
+	String getLastMatricule();
+	
+	Etudiant findOneByMatricule(String matricule);
 }
