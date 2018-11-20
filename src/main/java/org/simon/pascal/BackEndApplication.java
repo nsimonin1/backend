502bffc32393a7ca6bed29d463f01f462169eeb5
simonpascal <simon.pascal.ngos@accenture.com>
2018-11-20 21:14:17 +0400

    add cross domain

--- src/main/java/org/simon/pascal/BackEndApplication.java
+++ src/main/java/org/simon/pascal/BackEndApplication.java
@@ -1,12 +1,30 @@
 package org.simon.pascal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BackEndApplication {
-
+	private final Logger log=LoggerFactory.getLogger(getClass());
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
 	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        if (config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty()) {
+            log.debug("Registering CORS filter");
+            source.registerCorsConfiguration("/api/**", config);
+            source.registerCorsConfiguration("/management/**", config); 
+        }
+        return new CorsFilter(source);
+    }
 }
